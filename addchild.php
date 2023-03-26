353c63c7ba864c18edc84b4858f712a34ccef406
Praveentech21 <ravikuar_csd@srkrec.edu.in>
2023-03-26 23:30:43 +0530

    create a Asses name with Bhavani SHiva

--- addchild.php
+++ addchild.php
@@ -26,7 +26,7 @@ if(isset($_POST['submit'])){
   <head>
     <meta charset="UTF-8">
     <!---<title> Responsive Registration Form | CodingLab </title>--->
-    <link rel="stylesheet" href="hello.css">
+    <link rel="stylesheet" href="Bhavani/css/hello.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
 <body>

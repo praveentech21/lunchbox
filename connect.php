309374ad9ef943798bc2c20c22297af07df15ce3
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-06-25 22:42:06 +0530

    removed every thing shiva

--- connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = new mysqli('localhost','root','','lunchbox');
-?>
-<!-- All Setted Shiva and this is sending to git as All set SHiva -->
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-unset($_SESSION['uname']);
-session_destroy();
-header("location:index.html");
-
-?>
-<!-- All Setted Shiva and this is sending to git as All set SHiva -->

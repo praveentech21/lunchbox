3894552bc9b9147afc5c51054bc1b346d146a33a
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-08-15 15:48:24 +0530

    some files haders are diff shiva

--- admin/delivery_agent_students.php
+++ admin/delivery_agent_students.php
@@ -1,66 +1,248 @@
 <!DOCTYPE html>
 <?php
 session_start();
-if (empty($_SESSION['eid'])) {
-  header("location:login.php");
-}
+if(empty($_SESSION['eid']))
+{
+    header("location:login.php");
+} 
 include("connect.php");
-$eid = $_SESSION['eid'];
-$run1 = mysqli_fetch_assoc(mysqli_query($con, "select * from team where eid='$eid'"));
-$run2 = mysqli_query($con, "select * from subscriptions where delivery_partner='$eid'");
+$eid=$_SESSION['eid'];
+$run1 = mysqli_fetch_assoc(mysqli_query($con,"select * from team where eid='$eid'"));
+$run2 = mysqli_query($con,"select * from subscriptions where delivery_partner='$eid'");
 ?>
-<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="Bhavani/" data-template="vertical-menu-template-free">
+<html
+  lang="en"
+  class="light-style layout-menu-fixed"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="Bhavani/"
+  data-template="vertical-menu-template-free"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
 
-<head>
-  <?php include 'bhavani.php'; ?>
-</head>
+    <title>Delivery Agent Students</title>
 
-<body>
-  <?php include 'header.php'; ?>
+    <meta name="description" content="" />
 
-  <!-- Content wrapper -->
-  <div class="content-wrapper">
-    <!-- Content -->
-    <div class="container-xxl flex-grow-1 container-p-y">
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="Bhavani/img/favicon/favicon.ico" />
 
-    </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet"
+    />
 
-    <!-- / Content -->
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="Bhavani/vendor/fonts/boxicons.css" />
 
-    <!-- Footer -->
-    <footer class="content-footer footer bg-footer-theme">
-      <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-        <div class="mb-2 mb-md-0">
-          ©
-          <script>
-            document.write(new Date().getFullYear());
-          </script>
-          , made with ❤️ by
-          <a href="https://saipraveen.tech" target="_blank" class="footer-link fw-bolder">Sai Praveen</a>
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="Bhavani/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="Bhavani/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="Bhavani/css/demo.css" />
+    <link rel="stylesheet" href="Bhavani/css/style.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="Bhavani/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+    <link rel="stylesheet" href="Bhavani/vendor/libs/apex-charts/apex-charts.css" />
+
+    <!-- Page CSS -->
+
+    <!-- Helpers -->
+    <script src="Bhavani/vendor/js/helpers.js"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="Bhavani/js/config.js"></script>
+  </head>
+
+  <body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <!-- Menu -->
+
+        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+          <div class="app-brand demo">
+            <span class="app-brand-logo demo">
+              <img src="Bhavani/img/logo.png" alt="Lunch Box" class="img-fluid" />
+            </span>
+          </div>
+
+          <div class="menu-inner-shadow"></div>
+
+          <ul class="menu-inner py-1">
+            <!-- Dashboard -->
+            <li class="menu-item ">
+              <a href="delivary.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Dashboard</div>
+              </a>
+            </li>
+            <li class="menu-item ">
+              <a href="delivery_agent_profile.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-user "></i>
+                <div data-i18n="Analytics">Profile</div>
+              </a>
+            </li>
+            <li class="menu-item active">
+              <a href="delivery_agent_students.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bxl-baidu "></i>
+                <div data-i18n="Analytics">Students</div>
+              </a>
+            </li>
+
+          </ul>
+        </aside>
+        <!-- / Menu -->
+
+        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Navbar -->
+
+          <nav
+            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+            id="layout-navbar">
+
+            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+              <h2 style="padding-top: 18px;">Lunch Box</h2>
+              <ul class="navbar-nav flex-row align-items-center ms-auto">
+                <!-- User -->
+                <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                  <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                    <div class="avatar avatar-online">
+                      <img src="Bhavani/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                    </div>
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-end">
+                    <li>
+                      <a class="dropdown-item" href="delivary.php">
+                        <div class="d-flex">
+                          <div class="flex-shrink-0 me-3">
+                            <div class="avatar avatar-online">
+                              <img src="Bhavani/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                            </div>
+                          </div>
+                          <div class="flex-grow-1">
+                            <span class="fw-semibold d-block">John Doe</span>
+                            <small class="text-muted">Admin</small>
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="delivery_agent_profile.php">
+                        <i class="bx bx-user me-2"></i>
+                        <span class="align-middle">My Profile</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="delivery_agent_students.php">
+                        <i class="bx bxl-baidu me-2"></i>
+                        <span class="align-middle">Students</span>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="logout.php">
+                        <i class="bx bx-power-off me-2"></i>
+                        <span class="align-middle">Log Out</span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <!--/ User -->
+              </ul>
+            </div>
+          </nav>
+
+          <!-- / Navbar -->
+
+          <!-- Content wrapper -->
+          <div class="content-wrapper">
+            <!-- Content -->     
+            <div class="container-xxl flex-grow-1 container-p-y">
+
+            </div>
+
+            <!-- / Content -->
+
+            <!-- Footer -->
+            <footer class="content-footer footer bg-footer-theme">
+              <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+                <div class="mb-2 mb-md-0">
+                  ©
+                  <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                  , made with ❤️ by
+                  <a href="https://saipraveen.tech" target="_blank" class="footer-link fw-bolder">Sai Praveen</a>
+                </div>
+              </div>
+            </footer>
+            <!-- / Footer -->
+
+            <div class="content-backdrop fade"></div>
+          </div>
+          <!-- Content wrapper -->
         </div>
+        <!-- / Layout page -->
       </div>
-    </footer>
-    <!-- / Footer -->
 
-    <div class="content-backdrop fade"></div>
-  </div>
-  <!-- Content wrapper -->
-  <script>
-    function change_status(stdid, eid) {
-      alert("Function Called" + stdid + " " + eid);
-      // axaj call to pickup.php
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.responseText);
-        }
-      };
-      xhttp.open("POST", "update_status.php", true);
-      var data = "stdid=" + stdid + "&eid=" + eid;
-      xhttp.send(data);
-    }
-  </script>
-  <?php include 'fotter.php'; ?>
-</body>
+      <!-- Overlay -->
+      <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
 
+
+    <!-- Core JS -->
+    <!-- build:js vendor/js/core.js -->
+    <script src="Bhavani/vendor/libs/jquery/jquery.js"></script>
+    <script src="Bhavani/vendor/libs/popper/popper.js"></script>
+    <script src="Bhavani/vendor/js/bootstrap.js"></script>
+    <script src="Bhavani/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="Bhavani/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="Bhavani/vendor/libs/apex-charts/apexcharts.js"></script>
+
+    <!-- Main JS -->
+    <script src="Bhavani/js/main.js"></script>
+
+    <!-- Page JS -->
+    <script src="Bhavani/js/dashboards-analytics.js"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <script>
+      function change_status(stdid,eid){
+        alert("Function Called"+stdid+" "+eid);
+        // axaj call to pickup.php
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+           console.log(this.responseText);
+          }
+        };
+        xhttp.open("POST", "update_status.php", true);
+        var data = "stdid="+stdid+"&eid="+eid;
+        xhttp.send(data);
+      }
+    </script>
+  </body>
 </html>
--- admin/parent_registration_details.php
+++ admin/parent_registration_details.php
@@ -9,6 +9,44 @@ include("connect.php");
 
 <head>
   <?php include 'bhavani.php'; ?>
+  <style>
+/* Styles for the backdrop */
+.backdrop {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 100;
+}
+
+/* Styles for the popup */
+.popup {
+    display: none;
+    position: fixed;
+    top: 50%;
+    left: 50%;  
+    transform: translate(-50%, -50%);
+    background-color: white;
+    padding: 20px;
+    border: 1px solid #ccc;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    z-index: 101;
+    max-width: 90%; /* Set a maximum width for the popup */
+    width: auto; /* Allow the popup to expand to content width */
+}
+
+/* Media query for responsiveness */
+@media (max-width: 768px) {
+    .popup {
+        width: 90%; /* Adjust width for smaller screens */
+    }
+}
+
+
+  </style>
 </head>
 
 <body>
@@ -47,8 +85,65 @@ include("connect.php");
                   <td><a href="tel:<?php echo $run1['pid'] ?>"><?php echo $run1['pid'] ?></a></td>
                   <td><?php echo $address['area'] ?></td>
                   <td><?php echo $row['count(*)'] ?></td>
-                  <td><a href=""><span class="badge bg-label-info me-1">View Profile</span></a></td>
+                  <td><button id="editButton">View Profile</button></td>
                 </tr>
+                <div id="popupBackdrop" class="backdrop">
+                  <div id="popupForm" class="popup">
+                    <div class="row">
+
+                      <div class="col-xl">
+                        <div class="card mb-4">
+                          <div class="card-header d-flex justify-content-between align-items-center">
+                            <h5 class="mb-0">Update Parent Details</h5>
+                          </div>
+                          <div class="card-body">
+                            <form method="post" action="#">
+                              <div class="mb-3">
+                                <label class="form-label" for="basic-icon-default-fullname">Parent Name</label>
+                                <div class="input-group input-group-merge">
+                                  <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-user"></i></span>
+                                  <input type="text" class="form-control" id="basic-icon-default-fullname" placeholder="<?php echo $run1['pname']; ?>" aria-label="<?php echo $run1['pname']; ?>" aria-describedby="basic-icon-default-fullname2" name="pname" />
+                                </div>
+                              </div>
+                              <div class="mb-3">
+                                <label class="form-label" for="basic-icon-default-phone">Phone No</label>
+                                <div class="input-group input-group-merge">
+                                  <span id="basic-icon-default-phone2" class="input-group-text"><i class="bx bx-phone"></i></span>
+                                  <input type="number" id="basic-icon-default-phone" class="form-control phone-mask" placeholder="<?php echo $run1['pid']; ?>" aria-label="<?php echo $run1['pid']; ?>" aria-describedby="basic-icon-default-phone2" name="pid" />
+                                </div>
+                              </div>
+                              <div class="mb-3">
+                                <label class="form-label" for="basic-icon-default-phone">Alternative Phone</label>
+                                <div class="input-group input-group-merge">
+                                  <span id="basic-icon-default-phone2" class="input-group-text"><i class="bx bx-phone"></i></span>
+                                  <input type="number" id="basic-icon-default-phone" class="form-control phone-mask" placeholder="<?php echo $run1['altphone']; ?>" aria-label="658 799 8941" aria-describedby="basic-icon-default-phone2" name="altphone" />
+                                </div>
+                              </div>
+                              <div class="mb-3">
+                                <label class="form-label" for="basic-icon-default-email">Email</label>
+                                <div class="input-group input-group-merge">
+                                  <span class="input-group-text"><i class="bx bx-envelope"></i></span>
+                                  <input type="email" id="basic-icon-default-email" class="form-control" placeholder="<?php echo $run1['email']; ?>" aria-label="john.doe" aria-describedby="basic-icon-default-email2" name="email" />
+                                </div>
+                              </div>
+                              <div class="mb-3">
+                                <label class="form-label" for="basic-icon-default-fullname">Password</label>
+                                <div class="input-group input-group-merge">
+                                  <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-lock-open-alt"></i>
+                                  </span>
+                                  <input type="password" class="form-control" id="basic-icon-default-fullname" placeholder="..........." aria-label="John Doe" aria-describedby="basic-icon-default-fullname2" name="pass" />
+                                </div>
+                              </div>
+                              <button type="submit" name="update" class="btn btn-primary">Update</button>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>
+
+                  </div>
+                </div>
               <?php } ?>
             </tbody>
           </table>
@@ -78,6 +173,28 @@ include("connect.php");
 
   </div>
   <!-- Content wrapper -->
+  <script>
+    // Get references to the button and the popup elements
+const editButton = document.getElementById("editButton");
+const popupBackdrop = document.getElementById("popupBackdrop");
+const popupForm = document.getElementById("popupForm");
+
+// Add an event listener to the button
+editButton.addEventListener("click", function() {
+    // Show the popup by changing the display styles
+    popupBackdrop.style.display = "block";
+    popupForm.style.display = "block";
+});
+
+// Close the popup when clicking on the backdrop
+popupBackdrop.addEventListener("click", function() {
+    // Hide the popup by changing the display styles
+    popupBackdrop.style.display = "none";
+    popupForm.style.display = "none";
+});
+
+  </script>
+
   <?php include 'fotter.php'; ?>
 </body>
 

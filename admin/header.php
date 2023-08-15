09caeef7117a2701bb537d00ad5db72a461810bd
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-08-15 12:03:35 +0530

    header and fotter setted shiva

---
+++ admin/header.php
@@ -0,0 +1,196 @@
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <!-- Menu -->
+
+        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+          <div class="app-brand demo ">
+            <a href="index.php" class="app-brand-link">
+              <span class="app-brand-text demo menu-text fw-bolder ms-2">Lunch Box</span>
+            </a>
+          </div>
+
+          <div class="menu-inner-shadow"></div>
+
+          <ul class="menu-inner py-1">
+            <!-- Dashboard -->
+            
+            <!-- Layouts -->
+            <li class="menu-item">
+              <a href="index.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Dashboard</div>
+              </a>
+            </li>
+
+            <li class="menu-item">
+              <a href="allocate_student.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-lock-open-alt"></i>
+                <div data-i18n="Analytics">Allocate</div>
+              </a>
+            </li>
+
+            <li class="menu-header small text-uppercase">
+              <span class="menu-header-text">Our Leads</span>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-crown"></i>
+                <div data-i18n="Account Settings">Parents</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="parent_registration_details.php" class="menu-link">
+                    <div data-i18n="Account">Parent Registrations</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="update_parent_details.php" class="menu-link">
+                    <div data-i18n="Notifications">Update Parent Details</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="update_parent_address.php" class="menu-link">
+                    <div data-i18n="Connections">Update Parent Address</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            
+            <!-- Components -->
+            <!-- <li class="menu-header small text-uppercase"> -->
+              <!-- <span class="menu-header-text">Student Details</span></li> -->
+            <!-- Cards -->
+            <!-- User interface -->
+            <li class="menu-item">
+              <a href="javascript:void(0)" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-user"></i>
+                <div data-i18n="User interface">Students</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="all_student_list.php" class="menu-link">
+                    <div data-i18n="Accordion">All Student Details</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="student_wise_orders.php" class="menu-link">
+                    <div data-i18n="Alerts">Student Wise Analysis</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="update_student_subscription.php" class="menu-link">
+                    <div data-i18n="Badges">Update Subscription</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="allocate_delivary_agent.php" class="menu-link">
+                    <div data-i18n="Typography">Allocating Delivary Agent</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <!-- Extended components -->
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-support"></i>
+                <div data-i18n="Form Elements">Delivary</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="delivary_agent_details.php" class="menu-link">
+                    <div data-i18n="Basic Inputs">Delivary Agent Details</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="new_delivary_agent.php" class="menu-link">
+                    <div data-i18n="Basic Inputs">New Delivary Agent</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="delivary_agent_report.php" class="menu-link">
+                    <div data-i18n="Basic Inputs">Delivary Agent Report</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="updating_delivary_agent_link.php" class="menu-link">
+                    <div data-i18n="Input groups">Update Agent Link</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            
+            <!-- Misc -->
+            <!-- <li class="menu-header small text-uppercase"> -->
+              <!-- <span class="menu-header-text">Schools</span></li> -->
+              <li class="menu-item ">
+                <a href="newschool.php" class="menu-link">
+                  <i class="menu-icon tf-icons bx bx-cube-alt"></i>
+                  <div data-i18n="Analytics">New School</div>
+                </a>
+              </li>
+            
+          </ul>
+        </aside>
+        <!-- / Menu -->
+
+        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Navbar -->
+
+          <nav
+            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+            id="layout-navbar" >
+            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+              <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                <i class="bx bx-menu bx-sm"></i>
+              </a>
+            </div>
+
+            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+              
+              <h3 style="padding-top: 20px;">Bhimavaram Online</h3>
+
+              <ul class="navbar-nav flex-row align-items-center ms-auto">
+                <!-- User -->
+                <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                  <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                    <div class="avatar avatar-online">
+                      <img src="Bhavani/img/avatars/sureshsir.png" alt class="w-px-40 h-auto rounded-circle" />
+                    </div>
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-end">
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        <div class="d-flex">
+                          <div class="flex-shrink-0 me-3">
+                            <div class="avatar avatar-online">
+                              <img src="Bhavani/img/avatars/sureshsir.png" alt class="w-px-40 h-auto rounded-circle" />
+                            </div>
+                          </div>
+                          <div class="flex-grow-1">
+                            <span class="fw-semibold d-block">Dr. Suresh Babu</span>
+                            <small class="text-muted">Founder BVRMOL</small>
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                    
+                    <li>
+                      <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="logout.php">
+                        <i class="bx bx-power-off me-2"></i>
+                        <span class="align-middle">Log Out</span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <!--/ User -->
+              </ul>
+            </div>
+          </nav>
+
+          <!-- / Navbar -->
